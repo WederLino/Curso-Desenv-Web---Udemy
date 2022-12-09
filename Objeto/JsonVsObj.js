36fb827c4c45c078ea4864505773683d52fdd215
unknown <wederlinodesouza@gmail.com>
2022-12-09 18:43:13 -0300

    Diferença de obj para Json e o contrário

---
+++ Objeto/JsonVsObj.js
@@ -0,0 +1,6 @@
+const obj = {a: "Weder", b: 1, c: 2, soma(){ a + b }};
+
+console.log(JSON.stringify(obj));
+
+console.log(JSON.parse('{"a": "Lino", "b": 3, "c": 4, "d": {}, "e": [], "f": false}'));
+
