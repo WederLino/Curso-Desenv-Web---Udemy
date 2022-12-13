16a03b1133a3215c0e17e6beba33210800afabb6
unknown <wederlinodesouza@gmail.com>
2022-12-13 17:02:31 -0300

    Manipulando array usando concat()

---
+++ Array/Concat.js
@@ -0,0 +1,9 @@
+const array1 = ["Weder", "Lino"]
+const array2 = ["Maria", "Madalena"]
+
+const arrayConcatenado = array1.concat(array2, "PassandoUmElemento");
+
+console.log(arrayConcatenado);
+
+console.log(arrayConcatenado, array1, array2);
+
