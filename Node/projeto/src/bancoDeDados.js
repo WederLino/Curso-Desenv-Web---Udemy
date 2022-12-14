c690d82b9965dbcb2e0ede1ffd97db9016b31d7f
unknown <wederlinodesouza@gmail.com>
2022-12-14 18:48:17 -0300

    Usando express e os verbos get e post para manipular requisição e resposta com o postman

---
+++ Node/projeto/src/bancoDeDados.js
@@ -0,0 +1,14 @@
+module.exports = {
+    db: [
+            {
+                id: 1,
+                nome: "Weder",
+                idade: 21
+            },
+            {
+                id: 2,
+                nome: "Maria",
+                idade: 99
+            }
+        ]
+}
--- Node/projeto/src/servidor.js
+++ Node/projeto/src/servidor.js
@@ -1,11 +1,35 @@
+const dados = require("./bancoDeDados");
+const bodyParser = require('body-parser');
+
 const port = 3009;
 
-const express = require('express')
+const express = require('express');
 
 const app = express();
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.listen(port);
 
-app.get("/produtos", (req, resp) =>{
-    resp.send({nome: "Camaro", preco: 999})
+app.get("/pessoas", (req, resp) =>{
+    resp.send(dados.db)
 });
+
+app.get("/pessoa/:id", (req, res) =>{
+        let id = parseInt(req.params.id);
+        
+        let pessoa = dados.db.find(pessoa => pessoa.id == id);
+
+        res.send(pessoa);
+})
+
+app.post("/pessoas", (req, resp) =>{
+    
+    let pessoa = req.body;
+
+    dados.db.push(pessoa);
+
+    console.log(dados.db)
+    
+})
+
