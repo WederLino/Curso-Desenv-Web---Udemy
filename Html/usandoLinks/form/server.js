945f4d2126f82246061c34934c829daeeba9eb5e
unknown <wederlinodesouza@gmail.com>
2022-12-20 15:31:33 -0300

    Começo do uso do formulário em conjunto com express

---
+++ Html/usandoLinks/form/server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+const port = 4003;
+
+const db = []
+
+app.use(express.urlencoded({ extended: false }))
+
+app.post("/usuarios", (req, res) =>{
+
+    let usuario = req.body;
+
+    db.push(usuario)
+
+    res.send("<h1>Ok</h1>")
+
+   console.log(req.body)
+
+    
+})
+
+app.listen(port);
+
